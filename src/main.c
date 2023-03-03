6d87617dab71876c70c4d2ed4f1e15a6080598eb
dxngre <dxngre001@myuct.ac.za>
2023-03-03 16:01:53 +0200

    3.2
    Added delay function
    initialised lcd screen
    In for loop, pushed the increasing age number to the lcd screen

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@ struct age_data{
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
+void delay(unsigned int microseconds);
 void main(void);                                                   //COMPULSORY
 
 // MAIN FUNCTION -------------------------------------------------------------|
@@ -33,9 +34,18 @@ void main(void)
 		my_age.year = 2000;
 		my_age.age = 22;
 
+		init_LCD();
+
 	while(1)
 	{
+		for(uint8_t i = 0;i <= 22; i ++){
 
+				delay(20000);
+				sprintf(num, "%d", i);
+				lcd_putstring(num);
+				delay(20000);
+				lcd_command(CLEAR);
+		}
 	}
 }
 
